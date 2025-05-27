62da270815c8710daaae5062f9a21f462bae35bd
Teo Calvo <teocalvo2@gmail.com>
2025-05-27 11:00:38 -0300

    remove as queries

--- src/query/points/customers.sql
+++
@@ -1,7 +0,0 @@
-SELECT 
-    uuid As customer_id,
-    nr_points,
-    created_at,
-    updated_at
-
-FROM customers
--- src/query/points/transactions.sql
+++
@@ -1,8 +0,0 @@
-SELECT
-	UUID AS transaction_id,
-	id_customer,
-	created_at,
-	vl_points,
-	desc_sys_origin
-
-FROM transactions
